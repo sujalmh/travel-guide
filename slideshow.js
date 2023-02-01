9417cb7242f7eb843b3fc5d480de3e0519c1f175
sujalmh <63709163+sujalmh@users.noreply.github.com>
2023-02-01 22:01:02 +0530

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,15 +1,22 @@
 var slideIndex = ["images/8.jpg","images/9.jpg","images/11.jpg","images/12.jpg","images/13.jpg","images/4.jpg","images/14.jpg","images/15.jpg" ];
 var i =0;
+window.setInterval(update,3000)
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function update(){
     i++;
     x="url('"+slideIndex[i]+"')";
     document.getElementsByClassName("jumbotron")[0].style.backgroundImage=x;
+
     
     if(i==slideIndex.length){
         i=0;
         x="url('"+slideIndex[i]+"')";
-        document.getElementsByClassName("jumbotron")[0].style.backgroundImage=x;
+        
 
+        document.getElementById("slide").style.backgroundImage=x;
 
     }
      
@@ -19,15 +26,11 @@ function undo(){
     if(i==0){
         i=slideIndex.length;
         x="url('"+slideIndex[i]+"')";
-        document.getElementsByClassName("jumbotron")[0].style.backgroundImage=x;
-
-        }
+        
+        document.getElementById("slide").style.backgroundImage=x;  }
         
     
         i--;
         x="url('"+slideIndex[i]+"')";
-        document.getElementsByClassName("jumbotron")[0].style.backgroundImage=x;
-
-        
-
+        document.getElementById("slide").style.backgroundImage=x;        
 }
